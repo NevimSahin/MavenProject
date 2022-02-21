df918cb914dd64cc9526bdef9344f2451428d287
neyuc <nm.yucesoy@gmail.com>
2022-02-21 13:53:41 -0500

    share project

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -2,5 +2,7 @@ public class helloTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello team");
+        System.out.println("shared proojrct");
+
     }
 }
